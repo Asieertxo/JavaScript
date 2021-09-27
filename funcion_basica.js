ee04a6e96b79196bc8d63e391e95296c17307f21
Asieertxo <asgal.gl@gmail.com>
2021-09-27 20:24:32 +0200

    Intento ejercicios 2 y 3

--- funcion_basica.js
+++ funcion_basica.js
@@ -6,7 +6,7 @@ function greet(greeting, name){
 console.log(greet("Bye", "Peter"));
 */
 
-var numbers = [1, 4, 6];
+var numbers = [1, 4, 6, 3, 9, 12, 15, 10];
 
 function double(numbers){
     var result = [];
@@ -19,6 +19,30 @@ function double(numbers){
 
 console.log (double(numbers));
 
+
+function triple(numbers){
+    var result = [];
+    
+    for (var i = 0; i < numbers.length; i++){
+        result.push(numbers[i] * 3);
+    }
+    return result;
+}
+
+console.log (triple(numbers));
+
+
+function espar(numbers){
+    var resultado = [];
+    for (var i=0; i<numbers.length; i++){
+        if (numbers[i]%2 == 0){
+            resultado.push(true);
+        }else{
+            resultado.push(false);
+        }
+    }
+}
+console.log (espar(numbers));
 
 /*crear una funcion que multiplique por 3 los elementos de un array
 funcion que diga si los elemntos son pares o impares (ver video)*/
